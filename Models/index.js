949b7887a4c1da2f14fa4a00b1866530b5dd21d3
Mizuri <pro.1.h@outlook.com>
2022-11-21 12:41:32 -0700

    added code

---
+++ Models/index.js
@@ -0,0 +1,17 @@
+//username
+// string
+//unique
+//required 
+//trimmed
+
+//email
+//string
+//required
+//unique
+//must match a valid email address
+
+//thoughts
+//array of _id referencing thought model
+
+//friends
+//array of _id referencing user
---
+++ utils/seed.js
@@ -0,0 +1,9 @@
+const connection = require('../config/connection');
+const { User, Thought } = require('../models');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+    console.log('connected');
+    
+})
