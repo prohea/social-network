f5a0599cff1feddd5d4fd3f552733cef731a5a8a
Mizuri <pro.1.h@outlook.com>
2022-11-22 19:15:45 -0700

    updated server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const db = require('./config/connection');
 const routes = require('./routes');
-
-const cwd = process.cwd();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
